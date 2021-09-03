fe7d300b4c81182eb9391cf5eb311765e1d9207b
nagano.ryo <nagano.ryo@tis.co.jp>
2021-09-03 17:14:21 +0900

    Step 2: Implement the title row

--- building_layouts_tutorial/lib/main.dart
+++ building_layouts_tutorial/lib/main.dart
@@ -9,6 +9,44 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+
     return MaterialApp(
       // title: 'Welcome to Flutter',
       title: 'Flutter layout demo',
@@ -17,8 +55,13 @@ class MyApp extends StatelessWidget {
           // title: const Text('Welcome to Flutter'),
           title: const Text('Flutter layout demo'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        // body: const Center(
+        //   child: Text('Hello World'),
+        // ),
+        body: Column(
+          children: [
+            titleSection,
+          ],
         ),
       ),
     );
